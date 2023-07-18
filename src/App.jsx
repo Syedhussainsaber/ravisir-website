61f30f7bc7391837c18dc253abc3d155f38ecdb9
SyedHussainSaber <syedhussainsaber8@gmail.com>
2023-07-18 18:14:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import './App.css'
+import "./styles/navbar.scss"
+
+function App() {
+
+  return (
+    <>
+<Navbar/>
+<h1>Ravi Sir Website</h1>
+ </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+    <nav className="nav">
+<div className="nav__brand">
+    <h1>Dr. Ravi Chatterji</h1>
+</div>
+      <ul className={active}>
+      <li className="nav__item">
+          <a href={"/"}  className="nav__link">Home</a>
+        </li>
+        <li className="nav__item">
+          <a href={"/"}  className="nav__link">About Me</a>
+        </li>
+        <li className="nav__item">
+          <a href={"/"}  className="nav__link">What I Do</a>
+        </li>
+        <li className="nav__item">
+          <a href={"/"}  className="nav__link">Works</a>
+        </li>
+        <li className="nav__item">
+          <a href={"/"}  className="nav__link">You can find me here</a>
+        </li>
+        <li className="nav__item">
+        <a href={"/"}  className="nav__link">Testimonals</a>
+        </li>
+      
+<li className="nav__item"> <a href={"/"}  className="nav__link">Contact me</a></li>
+
+
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+
